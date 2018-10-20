fa1e0c0a1a9095fc914c04f2a7eff4e765214ede
p2008 <p2008@op.pl>
2018-10-20 14:30:58 +0200

    Modify models

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=64)
+    capacity = models.IntegerField()
+    is_projector = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    date = models.DateField()
+    room_id = models.ForeignKey(Room, on_delete=models.DO_NOTHING, name='room')
+    comment = models.TextField()
