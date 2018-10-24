87abac6bd738b5e2ca798c671050c8a57827b6fc
Jason Miller <likemoody@gmail.com>
2018-10-24 20:28:22 +0200

    Room details: overlapping dates.

--- rooms/view/room_details_view.py
+++ rooms/view/room_details_view.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from rooms.models import Room, Reservation
@@ -8,7 +9,8 @@ def room_details(request, rid):
     try:
         room = Room.objects.get(pk=rid)
         title = f'room {room.name}'
-        reservation_raw = Reservation.objects.filter(room_id=rid)
+        today = datetime.now().strftime('%Y-%m-%d')
+        reservation_raw = Reservation.objects.filter(room_id=rid).filter(date__gte=today)
         reservation_dates = [date.date for date in reservation_raw]
 
         return render(request, 'room_details_view.html',
